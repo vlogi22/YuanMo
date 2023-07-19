6ac5d4e04862926a4ed662123d198bbea0151337
Vlogi <luis200202@hotmail.com>
2023-07-19 18:04:29 +0100

    feat: game folder

---
+++ yuan_mo/game.h
@@ -0,0 +1,20 @@
+#ifndef __YUANMO_GAME_YUAN_MO_H__
+#define __YUANMO_GAME_YUAN_MO_H__
+
+namespace yuan_mo {
+
+    class game {
+
+    private:
+        /* data */
+    public:
+        game(/* args */) {
+        }
+        
+        ~game() {
+        }
+    };
+    
+}
+
+#endif
