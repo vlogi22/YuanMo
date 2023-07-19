b8349b40e88ff76013c6e469ba623ac74b48956c
Vlogi <luis200202@hotmail.com>
2023-07-19 18:04:07 +0100

    feat: app folder

---
+++ app/main/label.h
@@ -0,0 +1,11 @@
+#ifndef __YUANMO_APP_MAIN_LABEL_H__
+#define __YUANMO_APP_MAIN_LABEL_H__
+
+#include <string>
+
+namespace yuanmo::app {
+    
+    static const std::string COMMAND_NAME = "COMMAND_NAME";
+}
+
+#endif
