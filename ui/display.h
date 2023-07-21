be3811c517f0fbc3feb6addb35a7de446eca1b25
Vlogi <luis200202@hotmail.com>
2023-07-21 20:05:05 +0100

    feat(ui): add display component.
    the component which is reponsible for output informations.

---
+++ ui/display.h
@@ -0,0 +1,65 @@
+#ifndef __YUANMO_DISPLAY_H__
+#define __YUANMO_DISPLAY_H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace ui {
+
+    class display {
+
+    private:
+        std::string _text;
+
+    public:
+        display::display(std::string text = "") : 
+            _text(text) {
+        }
+        
+        display::~display() {
+        }
+
+        void display_text() {
+            std::cout << _text;
+            clear();
+        }
+
+        template<class T>
+        void add_line(T object, bool new_line = false) {
+            _text += object->to_string();
+            if (new_line) _text += "\n";
+        }
+
+        template<class T>
+        void add_vector(std::vector<T> vector) {
+            for (T object : vector) {
+                add_line(object, true);
+            }
+        }
+
+        void clear() {
+            _text = "";
+        }
+
+        template<class T>
+        void print(T object) {
+            if (object == nullptr) return ;
+            
+            add_line(object);
+            display_text();
+        }
+
+        template<class T>
+        void print_vector(std::vector<T> vector) {
+            if (object == nullptr) return ;
+
+            add_vector(vector);
+            display_text();
+        }
+
+    };
+    
+}
+
+#endif
