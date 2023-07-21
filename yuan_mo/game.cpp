d427f81c35caf9bda84c0ff733ad59916205d220
Vlogi <luis200202@hotmail.com>
2023-07-21 19:11:15 +0100

    feat: game class attributes

--- yuan_mo/game.cpp
+++ yuan_mo/game.cpp
@@ -1,1 +1,6 @@
 #include "game.h"
+
+yuan_mo::game::~game() {
+    delete _map;
+    delete _main_character;
+}
--- yuan_mo/game.h
+++ yuan_mo/game.h
@@ -1,17 +1,34 @@
-#ifndef __YUANMO_GAME_YUAN_MO_H__
-#define __YUANMO_GAME_YUAN_MO_H__
+#ifndef __YUANMO_GAME_H__
+#define __YUANMO_GAME_H__
+
+#include "maps/map.h"
+#include "maps/map_factory.h"
+#include "monsters/monster.h"
+#include "monsters/monster_factory.h"
 
 namespace yuan_mo {
 
     class game {
 
     private:
-        /* data */
+        int _lvl;
+        yuan_mo::map* _map;
+        yuan_mo::monster* _main_character;
+
     public:
-        game(/* args */) {
+        game(int lvl = 1) : _lvl(lvl) {
+            _map = yuan_mo::map_factory::create_map(lvl);
+            _main_character = yuan_mo::monster_factory::create_elite_monster(lvl);
         }
         
-        ~game() {
+        ~game();
+
+        yuan_mo::map* get_map() {
+            return _map;
+        }
+
+        yuan_mo::monster* get_character() {
+            return _main_character;
         }
     };
     
