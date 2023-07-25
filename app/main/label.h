97ec89e52be3ca6e3ee04deb4d875159f7b24574
QJL <jialiqiu01@gmail.com>
2023-07-25 01:52:44 +0100

    feat: update app

--- app/main/label.h
+++ app/main/label.h
@@ -3,9 +3,17 @@
 
 #include <string>
 
-namespace yuanmo::app {
-    
+namespace yuanmo::app::main {
+
     static const std::string COMMAND_NAME = "COMMAND_NAME";
+
+    static const std::string START_GAME = "START GAME";
+
+    static const std::string LOAD_GAME = "LOAD GAME";
+
+    static const std::string GAME_SETTINGS = "GAME SETTINGS";
+
+    static const std::string EXIT_GAME = "EXIT GAME";
 }
 
 #endif
